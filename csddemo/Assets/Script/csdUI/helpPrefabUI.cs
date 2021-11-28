b0190f9f33a78b4c06cede44b2866a7bb332e2c0
fuzhoustone <363910994@qq.com>
2021-11-28 15:52:56 +0800

    帮助界面

---
+++ csddemo/Assets/Script/csdUI/helpPrefabUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helpPrefabUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void UIShow() {
+        gameObject.SetActive(true);
+    }
+
+    public void UIClose() {
+        gameObject.SetActive(false);
+        //gameObject.active = false;
+    }
+}
--- csddemo/Assets/Script/startScene.cs
+++ csddemo/Assets/Script/startScene.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class startScene : MonoBehaviour
 {
     public Button continueBtn;
+    public GameObject helpObj;
     private bool hasRecord;
     void Start()
     {
