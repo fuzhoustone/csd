f4fd8d022e3a04e90211017e4c3969ef05b385d9
fuzhoustone <363910994@qq.com>
2022-01-24 14:24:55 +0800

    伤害公式更新，主界面加游戏名字

--- csddemo/Assets/Script/RoleDamageCal.cs
+++ csddemo/Assets/Script/RoleDamageCal.cs
@@ -22,11 +22,46 @@ namespace DamageCal
         public int DamageCal(roleProperty AttPro, roleProperty DefPro)
         {
             int damage = 1;
-            if (AttPro.attack > 0)
-                damage = AttPro.attack;
+            float tmpAttack = AttPro.attack - DefPro.def;
+            if (tmpAttack < 1.0f)
+                tmpAttack = 1.0f;
+            float tmpRes = attribRestraint(AttPro.element, DefPro.element);
+            float res = tmpAttack * tmpRes;
+
+            damage = Mathf.FloorToInt(res); //向下取整
 
             return damage;
         }
+
+        private const int ciEleNull = 0;
+        private const int ciEleFire = 1;
+        private const int ciEleWind = 2;
+        private const int ciEleWater = 3;
+        private const int ciEleEarth = 4;
+        private const int ciEleLight = 5;
+        private const int ciEleDark = 6;
+        private float attribRestraint(int attEle, int defEle) {
+            float res = 1.0f;
+            if (attEle == ciEleNull) //无属性不受克制关系
+                res = 1.0f;
+            else if ( ((attEle == ciEleWind) && (defEle == ciEleEarth))  //风克土
+                  ||((attEle == ciEleEarth) && (defEle == ciEleWater)) //  土克水，
+                  || ((attEle == ciEleWater) && (defEle == ciEleFire)) //水克火，
+                  || ((attEle == ciEleFire) && (defEle == ciEleWind)) //火克风
+                  || ((attEle == ciEleLight) && (defEle == ciEleLight)) //圣克暗，
+                    )
+                    res = 1.3f;  //克制伤害+30%
+            else if (((defEle == ciEleWind) && (attEle == ciEleEarth))  //风克土
+                  || ((defEle == ciEleEarth) && (attEle == ciEleWater)) //  土克水，
+                  || ((defEle == ciEleWater) && (attEle == ciEleFire)) //水克火，
+                  || ((defEle == ciEleFire) && (attEle == ciEleWind)) //火克风
+                  || ((defEle == ciEleLight) && (attEle == ciEleLight)) //圣克暗，
+                    )
+                res = 0.7f;
+
+            return res;
+        }
+
 
     }
 }
