fbb22f15e8ba9e0541acf76d6bcf8bc94f9000ff
fuzhoustone <363910994@qq.com>
2022-06-10 16:47:18 +0800

    接入steamSDK

---
+++ chineserpg/Assets/Scripts/SteamScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Steamworks;
+
+public class SteamScript : MonoBehaviour
+{
+    void Start()
+    {
+#if DEMO
+        Debug.LogWarning("this is demo run");
+#else
+        if (SteamManager.Initialized)
+        {
+            Debug.Log("steam sdk init success");
+
+            /*
+            string name = SteamFriends.GetPersonaName();
+            Debug.Log(name);
+            */
+        }
+#endif
+    }
+}
